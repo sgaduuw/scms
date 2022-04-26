5ca6ed68a4a9d9084dca3b0b4177a0c8064537cb
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-04-26 23:31:42 +0200

    Working through ming docs basics

--- scms/models.py
+++ scms/models.py
@@ -1,34 +1,31 @@
-from ming import create_datastore
-from ming.odm import ThreadLocalODMSession
-
-from ming import schema
-from ming.odm import MappedClass
-from ming.odm import FieldProperty, ForeignIdProperty
-
+from ming import Session, create_datastore
+from ming import Document, Field, schema
+#from ming.odm import MappedClass
+#from ming.odm import FieldProperty, ForeignIdProperty
+#from ming.odm import ThreadLocalODMSession
 from ming.odm import Mapper
 
-
-session = ThreadLocalODMSession(
-    bind=create_datastore('mim:///tutorial')
-)
-session
-
-class Site(MappedClass):
+session = Session(create_datastore('mongodb+srv://mongoacct01:TzT1xCaAkUs3mMUp@cms.poqcs.mongodb.net/dev?retryWrites=true&w=majority'))
+class Site(Document):
     class __mongometa__:
         session = session
         name = 'site'
-    
-    _id = FieldProperty(schema.ObjectId)
-    name = FieldProperty(schema.String(required=True))
 
-class WikiPage(MappedClass):
+    m: 'Manager[Site]'
+    
+    _id = Field(schema.ObjectId)
+    name = Field(str)
+
+class Page(Document):
     class __mongometa__:
         session = session
-        name = 'wiki_page'
+        name = 'page'
 
-    _id = FieldProperty(schema.ObjectId)
-    title = FieldProperty(schema.String(required=True))
-    text = FieldProperty(schema.String(if_missing=''))
+    m: 'Manager[Page]'
+
+    _id = Field(schema.ObjectId)
+    title = Field(str)
+    text = Field(str)
 
 Mapper.compile_all()
 
