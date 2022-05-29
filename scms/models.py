7833e4bfaf2951b6c3106ed5ebf408d7f7e8bbd9
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-05-29 13:38:55 +0200

    Rework models to use ODM and relate them

--- scms/models.py
+++ scms/models.py
@@ -1,34 +1,37 @@
+""" the models for the scms app """
 import os
-import datetime
-from ming import Session, create_datastore
-from ming import Document, Field, schema
-#from ming.odm import MappedClass
-#from ming.odm import FieldProperty, ForeignIdProperty
-#from ming.odm import ThreadLocalODMSession
-from ming.odm import Mapper
 
-session = Session(create_datastore(os.getenv('MONGO_CONNECT')))
-class Site(Document):
+from ming import create_datastore, schema
+from ming.odm import (FieldProperty, ForeignIdProperty, MappedClass, Mapper,
+                      RelationProperty, ThreadLocalODMSession)
+
+session = ThreadLocalODMSession(
+    bind=create_datastore(os.getenv('MONGO_CONNECT'))
+)
+
+class Site(MappedClass):
+    """ The model class for a site. """
     class __mongometa__:
         session = session
         name = 'site'
 
-    m: 'Manager[Site]'
-    
-    _id = Field(schema.ObjectId)
-    name = Field(str)
+    _id = FieldProperty(schema.ObjectId)
+    name = FieldProperty(schema.String)
+    # fqdns = FieldProperty(schema.Array)
 
-class Page(Document):
+    page = RelationProperty('Page')
+
+
+class Page(MappedClass):
+    """ The model class for a page. """
     class __mongometa__:
         session = session
         name = 'page'
 
-    m: 'Manager[Page]'
-
-    _id = Field(schema.ObjectId)
-    title = Field(str)
-    text = Field(str)
-    date = Field(datetime.datetime)
-    site = Field(schema.ObjectId)
+    _id = FieldProperty(schema.ObjectId)
+    title = FieldProperty(schema.String)
+    text = FieldProperty(schema.String)
+    date = FieldProperty(schema.DateTime)
+    site_id = ForeignIdProperty('Site')
 
 Mapper.compile_all()
