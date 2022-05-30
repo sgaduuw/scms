e7693a2ae72ef90512cfdc15a488147a0e8b130a
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-05-30 23:34:23 +0200

    Add more fields

--- scms/models.py
+++ scms/models.py
@@ -18,6 +18,10 @@ class Site(MappedClass):
     _id = FieldProperty(schema.ObjectId)
     name = FieldProperty(schema.String)
     fqdns = FieldProperty(schema.Array(str))
+    title = FieldProperty(schema.String)
+    tagline = FieldProperty(schema.String)
+    description = FieldProperty(schema.String)
+    copyright = FieldProperty(schema.String)
 
     page = RelationProperty('Page')
 
@@ -32,6 +36,17 @@ class Page(MappedClass):
     title = FieldProperty(schema.String)
     text = FieldProperty(schema.String)
     date = FieldProperty(schema.DateTime)
+
+    body = FieldProperty(schema.String)
+    slug = FieldProperty(schema.String)
+    publishdate = FieldProperty(schema.DateTime)
+    description = FieldProperty(schema.String)
+
+    # TODO
+    # tags
+    # categories
+    # content_type
+
     site_id = ForeignIdProperty('Site')
 
 Mapper.compile_all()
