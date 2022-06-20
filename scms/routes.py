2afce6b259e75f5330119439f84bab06e2c3d009
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-06-20 17:29:26 +0200

    Allow for creating sites and updating its metadata

--- scms/routes.py
+++ scms/routes.py
@@ -142,7 +142,47 @@ def normal_admin_single_site(site_name):
         return render_template('admin_normal_single_site.html', active_admin=False), 401
 
     else:
-        return render_template('admin_normal.html', active_admin=True)
+        from scms.admin_forms import SiteInfoForm
+
+        site = Site.query.get(name=site_name)
+        # this feels pretty perly, i like it!
+        form = SiteInfoForm() if site is None else SiteInfoForm(
+            name=site.name,
+            title=site.title,
+            tagline=site.tagline,
+            description=site.description,
+            copyright=site.copyright,
+            fqdns=str(",".join(site.fqdns))
+        )
+
+        if form.validate_on_submit():
+            site_name = form.name.data
+            site_fqdns = form.fqdns.data.split(',')
+            site_title = form.title.data
+            site_tagline = form.tagline.data
+            site_description = form.description.data
+            site_copyright = form.copyright.data
+
+            site = Site.query.find_and_modify(
+                query={'name': site_name},
+                update={
+                    '$set': {
+                        'name': site_name,
+                        'fqdns': site_fqdns,
+                        'title': site_title,
+                        'tagline': site_tagline,
+                        'description': site_description,
+                        'copyright': site_copyright
+                    }
+                },
+                upsert=True,
+                new=True
+            )
+        else:
+            print(f"NOT submitted.")
+            print(f"ERORS: {form.errors}")
+
+        return render_template('admin_normal_single_site.html', active_admin=True, site=site, form=form)
 
 
 @app.route('/admin/login', methods=['GET', 'POST'], strict_slashes=False)
