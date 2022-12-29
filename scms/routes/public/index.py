9731567913acf9a2779358894caa14e49e9cb449
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-12-29 14:30:53 +0100

    Display some content

--- scms/routes/public/index.py
+++ scms/routes/public/index.py
@@ -1,9 +1,16 @@
 from flask import render_template
 
+from scms.models import Content
 from scms.routes.public import public
 
 
 @public.route('/')
 def index():
 
-    return render_template('index.j2')
+    list_content = Content.objects
+
+    context = {
+        'list_content': list_content
+    }
+
+    return render_template('index.j2', **context)
