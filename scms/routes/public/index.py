f2bae0a9e3e7b8230868c7d2459bb2795a735227
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-12-30 22:57:37 +0100

    Show content based on the site selected by the Host header

--- scms/routes/public/index.py
+++ scms/routes/public/index.py
@@ -1,15 +1,20 @@
-from flask import render_template
+from flask import render_template, request
 
-from scms.models import Content
+from scms.models import Content, Site
 from scms.routes.public import public
 
 
 @public.route('/')
 def index():
-
-    list_content = Content.objects
+    # Quering the fqdns list in the Site model using fqdns__in
+    # requires a list to be passed.
+    host = request.host.rsplit(':', 1)[0]
+    site = Site.objects(fqdns__in=[host]).first()
+    # using site.id, we can determine our content query
+    list_content = Content.objects(site=site)
 
     context = {
+        'site': site,
         'list_content': list_content
     }
 
