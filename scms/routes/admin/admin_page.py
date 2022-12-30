f603875fbd7b1cc3dc0448ec437ed019cbeaca67
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-12-30 22:55:28 +0100

    Create mock content for random sites that are known in mongo

--- scms/routes/admin/admin_page.py
+++ scms/routes/admin/admin_page.py
@@ -6,7 +6,7 @@ from scms.models import User, Site, Content, Tag, Category
 from scms.routes.admin import admin
 
 import lorem
-
+import random
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -17,14 +17,20 @@ def load_user(user_id):
 @admin.route('/admin/')
 @login_required
 def admin_page():
+    site_names = ['Eelco01', 'Eelco02', 'Eelco03']
+    random_site = random.choice(site_names)
+    site = Site.objects(name=random_site).first()
     mock_content = {
+        'site': site.id,
         'title': lorem.get_word(count=5),
         'body': lorem.get_paragraph(count=7)
     }
+
     Content(**mock_content).save()
 
     context = {
         'sites': Site.objects,
+        'content': Content.objects,
         'tags': Tag.objects,
         'categories': Category.objects,
         'posts': Content.objects
