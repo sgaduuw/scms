7520512c5caecd2dd2e5318ee75e886db7acfb71
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2022-12-31 11:33:00 +0100

    Make the author required for content

--- scms/models/content.py
+++ scms/models/content.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from scms.extensions import db
-from scms.models import Site, Tag, Category
+from scms.models import Site, Tag, Category, User
 
 
 class Content(db.Document):
 
     title = db.StringField()
     body = db.StringField()
+    author = db.ReferenceField(User, required=True)
     create_date = db.DateTimeField(default=datetime.utcnow)
     publish_date = db.DateTimeField(default=datetime.utcnow)
     slug = db.StringField()
