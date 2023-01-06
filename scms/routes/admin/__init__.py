5748fbf708b2d1f0bc30d100f821208807e7df9a
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2023-01-06 15:02:20 +0100

    Make sure admin views have login_required

--- scms/routes/admin/__init__.py
+++ scms/routes/admin/__init__.py
@@ -1,28 +1,47 @@
 from flask import Blueprint
-# from scms.extensions import login_manager
-
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.mongoengine import ModelView
+from flask_login import login_required
 
+# from scms.extensions import login_manager
 from scms.models import User, Group, Permission, Site, Content, Category, Tag
+
+
+# Create a login-required decorator
+def login_required_admin(view):
+    @login_required
+    def wrapper(*args, **kwargs):
+        return view(*args, **kwargs)
+    return wrapper
+
 
 admin_bp = Blueprint(
     'admin',
     __name__
 )
+
+
+# Create a login-required AdminIndexView
+class LoginRequiredAdminIndexView(AdminIndexView):
+    @login_required_admin
+    def index(self):
+        return super(LoginRequiredAdminIndexView, self).index()
+
+
+# Create a login-required ModelView
+class LoginRequiredModelView(ModelView):
+    @login_required_admin
+    def is_accessible(self):
+        return super(LoginRequiredModelView, self).is_accessible()
+
+
+# Create an Admin instance
 admin = Admin(admin_bp, template_mode='bootstrap4')
 
-# @login_manager.user_loader
-# def load_user(user_id):
-
-#     return User.objects(pk=user_id).first()
-
-# from scms.routes.admin import index # noqa
-# from scms.routes.admin import sites # noqa
-admin.add_view(ModelView(Site))
-admin.add_view(ModelView(Content))
-admin.add_view(ModelView(Category))
-admin.add_view(ModelView(Tag))
-admin.add_view(ModelView(User))
-admin.add_view(ModelView(Group))
-admin.add_view(ModelView(Permission))
+admin.add_view(LoginRequiredModelView(Site))
+admin.add_view(LoginRequiredModelView(Content))
+admin.add_view(LoginRequiredModelView(Category))
+admin.add_view(LoginRequiredModelView(Tag))
+admin.add_view(LoginRequiredModelView(User))
+admin.add_view(LoginRequiredModelView(Group))
+admin.add_view(LoginRequiredModelView(Permission))
